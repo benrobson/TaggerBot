adcfdc96b3b3e9a07482a645fe628c3bdddfa8a0
shadowolf <shadowolfbusiness@gmail.com>
2018-04-10 13:20:27 +1000

    Renamed help command to tags.

--- commands/tags.js
+++ commands/tags.js
@@ -4,11 +4,10 @@ const config = require('../config.json');
 module.exports.run = async (client, message, args) => {
   const commandNames = Array.from(client.commands.keys());
   const longest = commandNames.reduce((long, str) => Math.max(long, str.length), 0);
-  message.channel.sendCode('asciidoc', `= Command List =\n\n${client.commands.map(c => `${config.prefix}${c.help.name}${' '.repeat(longest - c.help.name.length)} :: ${c.help.description}`).join('\n')}`);
+  message.channel.sendCode('asciidoc', `= Tags =\n\n${client.commands.map(c => `${config.prefix}${c.help.name}${' '.repeat(longest - c.help.name.length)} :: ${c.help.description}`).join('\n')}`);
 };
 
 module.exports.help = {
-  name: 'help',
-  description: 'Displays all commands and descriptions.',
-  usage: 'help'
+  name: 'tags',
+  description: 'Displays all tags & descriptions.'
 };
