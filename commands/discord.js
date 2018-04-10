2cdd5957f1143e285a951cbc2084c414511fb545
shadowolf <shadowolfbusiness@gmail.com>
2018-04-10 14:05:31 +1000

    Added discord command.

--- commands/discord.js
+++ commands/discord.js
@@ -2,12 +2,12 @@ const Discord = require('discord.js');
 
 module.exports.run = async (client, message, args) => {
   let embed = new Discord.RichEmbed()
-  .setImage('https://media.giphy.com/media/ZcYe7jWTLRzkQ/giphy.gif');
+  .setImage('https://media1.tenor.com/images/83cdd1dd40cdb87020949e0f075b9648/tenor.gif?itemid=11230336');
 
   return message.channel.send(embed);
 };
 
 module.exports.help = {
-  name: 'gay',
+  name: 'discord',
   description: ''
 };
